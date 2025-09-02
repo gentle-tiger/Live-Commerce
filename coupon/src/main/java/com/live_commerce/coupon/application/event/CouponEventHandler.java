15e190fd90cad8b4bb1a1e43d9057c310e0a032a
Choi Hojin <jkhh9725@gmail.com>
2025-09-02 22:54:48 +0900

    refactor: outbox 모드 활성화 조건 변경 - `coupon.outbox.enabled` 프로퍼티가 `false`일 때 Outbox 모드 비활성화 - 프로퍼티 미설정 시 기본값으로 Outbox 모드 활성화 (`matchIfMissing=true`)

--- coupon/src/main/java/com/live_commerce/coupon/application/event/CouponEventHandler.java
+++ coupon/src/main/java/com/live_commerce/coupon/application/event/CouponEventHandler.java
@@ -4,18 +4,19 @@ import com.live_commerce.coupon.application.port.out.PublishCouponUsedEventPort;
 import com.live_commerce.coupon.domain.event.CouponUsedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-/* DB 커밋 이후에만 외부 발행 (AFTER_COMMIT) */
-@ConditionalOnBean(PublishCouponUsedEventPort.class) //  덕분에, 프로듀서 어댑터 없으면 조용히 비활성화됨(좋은 패턴).
+@ConditionalOnProperty(name = "coupon-outbox-enabled", havingValue = "false", matchIfMissing = true)
 @Component
 @RequiredArgsConstructor
 public class CouponEventHandler {
 
   private final PublishCouponUsedEventPort publishCouponUsedEventPort; // port로 외부 발행
 
+  /* DB 커밋 이후에만 외부 발행 (AFTER_COMMIT) */
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void onCouponUsed(CouponUsedEvent event){
     publishCouponUsedEventPort.publishCouponUsedEvent(event.couponId(), event.userId());
